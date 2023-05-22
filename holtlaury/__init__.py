b5013eef0588fc23d2abb88f46577f242d7f90f0
Ted Turocy <ted.turocy@gmail.com>
2023-05-22 14:29:03 +0100

    Implement realisation of payoffs for Holt-Laury.

--- holtlaury/__init__.py
+++ holtlaury/__init__.py
@@ -1,3 +1,5 @@
+import random
+
 from otree.api import *
 
 
@@ -10,6 +12,10 @@ class C(BaseConstants):
     NAME_IN_URL = 'holtlaury'
     PLAYERS_PER_GROUP = None
     NUM_ROUNDS = 1
+    LOTTERY_PAYOFFS = {
+        "A": [Currency("2.00"), Currency("1.60")],
+        "B": [Currency("3.85"), Currency("0.10")]
+    }
 
 
 class Subsession(BaseSubsession):
@@ -22,19 +28,41 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     choice1 = models.StringField(
-        choices=["A", "B"]
+        choices=["A", "B"],
+        label="1-in-10"
     )
     choice2 = models.StringField(
-        choices=["A", "B"]
+        choices=["A", "B"],
+        label="2-in-10"
     )
     paid_choice = models.StringField()
     die_roll = models.IntegerField()
+
+    def choice_fields(self):
+        return ['choice1', 'choice2']
+
+    def determine_outcome(self):
+        self.die_roll = random.randint(0, 9)
+        self.paid_choice = random.choice(self.choice_fields())
+        chosen_lottery = getattr(self, self.paid_choice)
+        target_roll = int(self.paid_choice.replace("choice", ""))
+        if self.die_roll < target_roll:
+            self.payoff = C.LOTTERY_PAYOFFS[chosen_lottery][0]
+        else:
+            self.payoff = C.LOTTERY_PAYOFFS[chosen_lottery][1]
 
 
 # PAGES
 class DecisionPage(Page):
     form_model = 'player'
-    form_fields = ['choice1', 'choice2']
+
+    @staticmethod
+    def get_form_fields(player: Player):
+        return player.choice_fields()
+
+    @staticmethod
+    def before_next_page(player: Player, timeout_happened):
+        player.determine_outcome()
 
 
 class ResultsWaitPage(WaitPage):
